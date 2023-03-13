8a5e2a196515642841d7f01e2de77a9719e38299
boborova3 <jan.boborova@gmail.com>
2023-03-13 15:42:00 +0100

    scripts updated to 5 iterations

--- KR 2023/eval_1_mhs-mxp_noNeg.sh
+++ KR 2023/eval_1_mhs-mxp_noNeg.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-for j in {0..9}; do for i in lubm-0_{1..5}_{0..9}_noNeg;do java -Xmx4096m -jar mhs-mxp.jar eval_1/mhs-mxp/"$i".in;done;done
+for j in {0..4}; do for i in lubm-0_{1..5}_{0..9}_noNeg;do java -Xmx4096m -jar mhs-mxp.jar eval_1/mhs-mxp/"$i".in;done;done
