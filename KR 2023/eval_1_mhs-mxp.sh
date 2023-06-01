9cb6cc736d2cdc9a6478c12b8a0136c5ee19a9fd
boborova3 <61248271+boborova3@users.noreply.github.com>
2023-06-01 22:17:51 +0200

    Delete KR 2023 directory

--- KR 2023/eval_1_mhs-mxp.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-for j in {0..4}; do for i in lubm-0_{1..5}_{0..9};do java -Xmx4096m -jar mhs-mxp.jar eval_1/mhs-mxp/"$i".in;done;done
--- KR 2023/eval_1_mhs_noNeg.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-for j in {0..4}; do
-	for i in lubm-0_{1..5}_{0..9}_noNeg; do
-		java -Xmx4096m -jar mhs-mxp.jar eval_1/mhs/"$i".in;
-	done;
-done
